98814f9bcb4973748781623f73cbb3f10b430e49
cjousset1966-cell <cjousset1966@gmail.com>
2026-08-05 16:19:27 +0200

    Enable mobile navigation menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// PJCG Scripts
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+if (menuToggle && mainNav) {
+  menuToggle.addEventListener("click", () => {
+    const isOpen = mainNav.classList.toggle("is-open");
+
+    menuToggle.setAttribute("aria-expanded", String(isOpen));
+  });
+}

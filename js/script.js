e44ade02e0a0df897d6be4e2e20ee5d66277927f
cjousset1966-cell <cjousset1966@gmail.com>
2026-08-17 15:11:39 +0200

    Close mobile menu after navigation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ const mainNav = document.querySelector(".main-nav");
 if (menuToggle && mainNav) {
   menuToggle.addEventListener("click", () => {
     const isOpen = mainNav.classList.toggle("is-open");
-
     menuToggle.setAttribute("aria-expanded", String(isOpen));
   });
+
+  mainNav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mainNav.classList.remove("is-open");
+      menuToggle.setAttribute("aria-expanded", "false");
+    });
+  });
 }
+
